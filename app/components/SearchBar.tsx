52cecbca2361a40b19e0cca9d2ecd5826bf13d54
Stephanie <me@stephaniemariewatson.com>
2023-04-28 10:10:16 -0400

    Implement Root Layout

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 
 export default function SearchBar() {
